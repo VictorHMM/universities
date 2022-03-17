6b0cda474bd178f58ccc370b96395da48cba8b6b
Victor Medeiros <victorh.mmedeiros@gmail.com>
2022-03-17 19:49:01 -0400

    Displays unis names in a list

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container class="pa-0">
+    <v-app-bar color="blue-grey lighten-1" prominent dark>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-toolbar-title>Universities</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar'
+
+};
+
+</script>
+
+<style scoped></style>
---
+++ src/components/shared/UnisList.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <ul>
+      <li v-for="uni in unisArray" :key="uni.name">{{ uni.name }}</li>
+    </ul>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "UnisList",
+  data: () => {
+    return {
+      unisArray: [
+        {
+          name: "Futureworks",
+          country: "United Kingdom",
+          webPages: ["https://futureworks.ac.uk/"],
+        },
+        {
+          name: "University of Arts of London",
+          country: "United Kingdom",
+          webPages: ["https://arts.ac.uk/"],
+        },
+        {
+          name: "University of Aberdeen",
+          country: "United Kingdom",
+          webPages: ["https://abdn.ac.uk/"],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
